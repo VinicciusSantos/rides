b81d4bd8942a52466df2ae39e57e16303cb8b60e
Viniccius Santos <comercial.viniciusguedes@gmail.com>
2024-11-23 19:35:42 -0300

    :recycle: refactor: wrapping home page into main folder

---
+++ client/app/(main)/layout.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import "../globals.css";
+
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import { store } from "@/store";
+import { Libraries, LoadScript } from "@react-google-maps/api";
+import localFont from "next/font/local";
+import { Provider as ReduxProvider } from "react-redux";
+
+const googleImportedLibraries: Libraries = ["places"];
+
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ReduxProvider store={store}>
+            <LoadScript
+              googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
+              libraries={googleImportedLibraries}
+            >
+              {children}
+              <Toaster />
+            </LoadScript>
+          </ReduxProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ client/app/(main)/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { DriverList } from "@/components/driver-list";
+import { Header } from "@/components/header";
+import { LocationForm } from "@/components/location-form";
+import { Card } from "@/components/ui/card";
+import { RootState } from "@/store";
+import dynamic from "next/dynamic";
+import { useSelector } from "react-redux";
+
+const Map = dynamic(() => import("@/components/map"), { ssr: false });
+
+export default function HomePage() {
+  const estimate = useSelector((state: RootState) => state.ride.estimate);
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main>
+        <Map mapContainerStyle={{ height: "24rem", width: "100%" }} />
+        <div className="flex justify-center w-full mb-40">
+          <Card className="w-1/3 max-w-[450px] absolute transform -translate-y-1/2 ">
+            <LocationForm />
+          </Card>
+        </div>
+
+        {estimate && (
+          <DriverList
+            drivers={estimate.options}
+            className="w-8/12 max-w-screen-lg m-auto"
+          />
+        )}
+      </main>
+    </div>
+  );
+}
