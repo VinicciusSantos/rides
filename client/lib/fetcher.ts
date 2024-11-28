22a6d6a6fe8317f52c4483567e34ac268966387f
Viniccius Santos <comercial.viniciusguedes@gmail.com>
2024-11-27 21:00:57 -0300

    :bug: fix: improve error handling in fetcher function

--- client/lib/fetcher.ts
+++ client/lib/fetcher.ts
@@ -1,18 +1,12 @@
-import { toast } from "@/hooks/use-toast";
 import { getAccessToken } from "@/services/auth.service";
 import { env } from "next-runtime-env";
-
-interface APIError {
-  error_code: string;
-  error_description: string;
-}
 
 export async function fetcher<T>(
   endpoint: string,
   options?: RequestInit
 ): Promise<T> {
   try {
-    const API_BASE_URL = env('NEXT_PUBLIC_API_BASE_URL');
+    const API_BASE_URL = env("NEXT_PUBLIC_API_BASE_URL");
     if (!API_BASE_URL) {
       throw new Error(
         "NEXT_PUBLIC_API_BASE_URL is not defined. Please check your .env configuration."
@@ -44,14 +38,7 @@ export async function fetcher<T>(
       return response as unknown as T;
     }
   } catch (error: unknown) {
-    toast({
-      title: "Request Error",
-      description:
-        (error as APIError).error_description || "Something went wrong.",
-      variant: "destructive",
-      duration: 4000,
-    });
-
+    console.error("Error fetching data:", error);
     throw error;
   }
 }
